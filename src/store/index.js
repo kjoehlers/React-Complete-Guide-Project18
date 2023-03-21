8fa898799f28c7c5931d980f79aa69068732daf4
Ken Oehlers <kjojr56@gmail.com>
2023-03-21 16:23:54 -0400

    Commit before Lesson 240

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,33 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = {
+  counter: 0,
+  showCounter: true,
+};
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "counter/increment") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "counter/increase") {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
   if (action.type === "counter/decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "showCounter/toggle") {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
     };
   }
 
